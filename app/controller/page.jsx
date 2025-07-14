ceecf6a7cc9ea37f4312f63b1ffd5632aa0e68d9
demaiodev <fml1041@gmail.com>
2025-07-14 12:44:02 -0400

    update the controller page for the last time ever maybe

--- app/controller/page.jsx
+++ app/controller/page.jsx
@@ -4,8 +4,9 @@ export default function Page() {
             <section className="flex flex-col gap-4">
                 <h1 className="text-3xl font-bold">Controller Configuration</h1>
                 <p className="text-lg">
-                    This page allows you to configure your controller settings.
+                    This page will allow you to configure your controller settings. For now it does absolutely nothing.
                 </p>
+                <p>I might even delete this in the future, who knows?</p>
             </section>
         </div>
     );
