5b5f03fc4555f5a48b66e5293336f96aab59ff4c
demaiodev <fml1041@gmail.com>
2025-07-14 20:17:08 -0400

    fixes

--- app/ewgf/page.jsx
+++ app/ewgf/page.jsx
@@ -4,15 +4,15 @@ export default function Page() {
             <section className="flex flex-col gap-4">
                 <h1 className="text-3xl font-bold">Electric Wind God Fist (EWGF)</h1>
                 <p>
-                    In the Tekken series, the Electric Wind God Fist (commonly called an "electric" or "EWGF") is the
-                    signature move of the Mishima family. The EWGF has the same input as the regular Wind God Fist, but
-                    is a "just frame" version, and will be executed instead of the Wind God Fist if the player inputs
-                    the d/f input and the 2 input on the exact same frame.
+                    In the Tekken series, the Electric Wind God Fist (commonly called an &quot;electric&quot; or
+                    &quot;EWGF&quot;) is the signature move of the Mishima family. The EWGF has the same input as the
+                    regular Wind God Fist, but is a &quot;just frame&quot; version, and will be executed instead of the
+                    Wind God Fist if the player inputs the d/f input and the 2 input on the exact same frame.
                 </p>
                 <p>
-                    Due to the EWGF's versatile nature and speed, it is a vital asset in every Mishima player's arsenal,
-                    especially for Kazuya. The EWGF is the safest launcher for Mishimas and is a useful punishing tool
-                    after whiffing an attack of your opponent.
+                    Due to the EWGF&apos;s versatile nature and speed, it is a vital asset in every Mishima
+                    player&apos;s arsenal, especially for Kazuya. The EWGF is the safest launcher for Mishimas and is a
+                    useful punishing tool after whiffing an attack of your opponent.
                 </p>
                 <p>
                     This app will help you practice the EWGF by showing you the input you need to perform, and
